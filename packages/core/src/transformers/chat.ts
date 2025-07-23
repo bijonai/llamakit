23fd718ab4abdd3840bfe200c8f1cc87ff551873
Acbox <acbox0328@gmail.com>
2025-07-24 01:28:13 +0800

    feat(core): completion request transform

--- packages/core/src/transformers/chat.ts
+++ packages/core/src/transformers/chat.ts
@@ -1,3 +1,44 @@
-export function createChatTransformer(
-  
-) {}
+import type { ChatCompletionResponse, ChatMessage, ChatRequestBody, Usage } from '../types/chat'
+import type { TransformerOptions } from './index'
+import { createTransform } from './index'
+
+export enum LogStatus {
+  PENDING = 'pending',
+  RUNNING = 'running',
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
+export type ChatLog = ({
+  status: LogStatus.PENDING | LogStatus.RUNNING
+  startAt: Date
+} | (({
+  status: LogStatus.SUCCESS
+  outputs: ChatMessage[]
+  usage: Usage
+} | {
+  status: LogStatus.ERROR
+  error: string
+}) & {
+  lastAt: Date
+  timeUsage: number
+})) & {
+  inputs: ChatMessage[]
+}
+
+export function createChatTransformer(options: TransformerOptions) {
+  return (body: ChatRequestBody, params: {
+    timestamp: Date
+  }) => createTransform<ChatRequestBody, ChatCompletionResponse, ChatLog>(options)(
+    body,
+    (req) => {
+      return {
+        status: LogStatus.PENDING,
+        startAt: params.timestamp,
+        inputs: req.messages,
+      }
+    },
+    (res) => {
+      return {}
+    }
+  )
+}
--- packages/core/src/transformers/index.ts
+++ packages/core/src/transformers/index.ts
@@ -3,4 +3,23 @@ export interface TransformerOptions {
   apiKey: string
 }
 
-export function createTransform(): (body: Reque) => {} {}
+export function createTransform<T extends object, R extends object, K extends object = T>(
+  _options: TransformerOptions,
+): (
+  body: T,
+  resolver?: (body: T) => K,
+) => [
+  K,
+  (response: R, params: object) => K,
+] {
+  return (
+    body: T,
+    resolver: (body: T) => K = (body) => body as unknown as K,
+    after: (body: R, params: object) => K = (body) => body as unknown as K,
+  ) => {
+    return [
+      resolver(body),
+      after,
+    ]
+  }
+}
