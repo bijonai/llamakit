bb83fb483a97a4a90b486f07d085f3879ef86d83
Lee <lee@mtftm.com>
2025-07-24 18:14:24 +0800

    style: apply lint fixes across multiple files (#1)
    * style: apply lint fixes across multiple files
    
    * style: update ESLint config ignores and modify event handler parameter

--- packages/core/src/transformers/index.ts
+++ packages/core/src/transformers/index.ts
@@ -6,16 +6,16 @@ export interface TransformerOptions {
 export function createTransform<T extends object, R extends object, K extends object = T>(
   _options: TransformerOptions,
 ): (
-  body: T,
-  resolver?: (body: T) => K,
-  after?: (body: R, o: K, params: object) => K,
-) => [
-  K,
-  (response: R, params: object) => K,
-] {
+    body: T,
+    resolver?: (body: T) => K,
+    after?: (body: R, o: K, params: object) => K,
+  ) => [
+    K,
+    (response: R, params: object) => K,
+  ] {
   return (
     body: T,
-    resolver: (body: T) => K = (body) => body as unknown as K,
+    resolver: (body: T) => K = body => body as unknown as K,
     after: (body: R, o: K, params: object) => K = (body, o) => o,
   ) => {
     return [
